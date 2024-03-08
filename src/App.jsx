1816b4aeb8267ac6af3d04401f3b375dda25ee62
Anna <cameaann@gmail.com>
2024-03-08 14:29:12 +0200

    2.6-2.7, 1-2 steps, added validation to prevent input the same names

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const handleOnChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const check = persons.find((x) => x.name === newName);
+    console.log(check);
+    if (check) {
+      alert(newName + " is already added to phonebook");
+    } else {
+      const newPerson = { 
+        name: newName, 
+        id: persons.length + 1 
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Phonebook</h2>
+      <ul>
+        {persons.map((x) => (
+          <li key={x.id}>{x.name}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleOnChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
