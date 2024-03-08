978399bee9dd50a0dae701ac0876f6359c15689a
Anna <cameaann@gmail.com>
2024-03-08 17:00:46 +0200

    2.9, step 9, added search field

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
 
+const initial = [
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+];
+
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas", number: "040-1234567" }]);
+  const [persons, setPersons] = useState(initial);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchWord, setSearch] = useState("");
+
+  const filteredPersons =
+    searchWord === ""
+      ? persons
+      : persons.filter((x) =>
+          x.name.toLowerCase().includes(searchWord.toLowerCase())
+        );
+  console.log(filteredPersons);
 
   const handleOnChange = (event) => {
     const { name, value } = event.target;
-    if(name === "name"){
+    if (name === "name") {
       setNewName(value);
     }
-    else if(name === "number"){
+    if (name === "number") {
       setNewNumber(value);
+    }
+    if (name === "search") {
+      setSearch(value);
     }
   };
 
@@ -25,7 +45,7 @@ const App = () => {
       const newPerson = {
         name: newName,
         id: persons.length + 1,
-        number: newNumber
+        number: newNumber,
       };
       setPersons(persons.concat(newPerson));
       setNewName("");
@@ -36,16 +56,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <ul>
-        {persons.map((x) => (
-          <li key={x.id}><span>{x.name}</span> {x.number}</li>
-        ))}
-      </ul>
+      <div>
+        filter shown with
+        <input value={searchWord} onChange={handleOnChange} name="search" />
+      </div>
       <form onSubmit={handleOnSubmit}>
         <div>
           name: <input value={newName} onChange={handleOnChange} name="name" />
           <div>
-          number: <input value={newNumber} onChange={handleOnChange} name="number" />
+            number:{" "}
+            <input value={newNumber} onChange={handleOnChange} name="number" />
           </div>
         </div>
         <div>
@@ -53,8 +73,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
-      <div>debug: {newName}</div>
+
+      <div>
+        {filteredPersons.map((x) => (
+          <div key={x.id}>
+            <span>{x.name}</span> {x.number}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
