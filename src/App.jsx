9f1a7fb02cf48f3cd6111df6e8998b0a8e38820b
Anna <cameaann@gmail.com>
2024-03-15 10:56:16 +0200

    added useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
-const initial = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
 
 const App = () => {
-  const [persons, setPersons] = useState(initial);
+  const [persons, setPersons] = useState([]);
   const [searchWord, setSearch] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+
+    axios.get("http://localhost:3001/persons")
+    .then((response) => {
+      setPersons(response.data);
+    })
+    
+  }, []);
 
   const filteredPersons =
     searchWord === ""
